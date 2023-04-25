e1958bcfc946072775748e7cc542b829be995c48
John <johnpk1007@gmail.com>
2023-04-25 16:38:27 +0900

    finally 11

--- server/models/redis.js
+++ server/models/redis.js
@@ -9,9 +9,6 @@ const redisEndpoint = process.env.REDIS_ENDPOINT;
 
 const redisClient = redis.createClient({
   url: `redis://${redisUsername}:${redisPassword}@${redisEndpoint}/0`,
-  socket: {
-    connectTimeout: 100000,
-  },
 });
 
 redisClient
