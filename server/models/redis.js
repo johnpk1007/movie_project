0bfb070bf7a99db251a22a319f8bf0228ddacfb1
John <johnpk1007@gmail.com>
2023-04-25 17:23:19 +0900

    finally 12

--- server/models/redis.js
+++ server/models/redis.js
@@ -9,11 +9,20 @@ const redisEndpoint = process.env.REDIS_ENDPOINT;
 
 const redisClient = redis.createClient({
   url: `redis://${redisUsername}:${redisPassword}@${redisEndpoint}/0`,
+  legacymode: true,
 });
 
-redisClient
-  .connect()
-  .then(() => console.log("Redis connected"))
-  .catch((error) => console.log("redisError:", error));
+// redisClient
+//   .connect()
+//   .then(() => console.log("Redis connected"))
+//   .catch((error) => console.log("redisError:", error));
+
+redisClient.on("connect", () => {
+  console.info("Redis connected!");
+});
+redisClient.on("error", (err) => {
+  console.error("Redis Client Error", err);
+});
+redisClient.connect();
 
 export default redisClient;
